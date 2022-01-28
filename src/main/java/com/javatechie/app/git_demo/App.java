fd01b22abc935b65e6142a1544705c7e0d943508
Subhrajeet Padhy <subhrajeet.padhy@gmail.com>
2022-01-28 15:24:46 +0530

    added git_demo project to local repo

---
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -0,0 +1,16 @@
+package com.javatechie.app.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public void addData(String data) {
+		System.out.println("Data =" + data);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
